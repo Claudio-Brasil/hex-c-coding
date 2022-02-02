2ae7a1398ede2dca075cfe22b6c06f9d500781cf
Claudio <clfjr2002@gmail.com>
2022-02-02 18:07:56 +0000

    exercise 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digit numbers of base 10 starting from 0
+ * Returns 0
+ */
+
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+printf("%d", number);
+
+printf("\n");
+
+return (0);
+}
