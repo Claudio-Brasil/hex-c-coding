4b5fdfb4380bb0057d8710d51e49075e127cea6f
Claudio <clfjr2002@gmail.com>
2022-02-02 18:12:46 +0000

    fiz description

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints all digit numbers of base 10 starting from 0
- * Returns 0
- */
+* main - Prints all digit numbers of base 10 starting from 0
+* Return: It returns 0
+*/
 
 int main(void)
 {
