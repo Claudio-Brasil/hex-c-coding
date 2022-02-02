46138fec0c8419fc4f8c6fd680902035a613f263
Claudio <clfjr2002@gmail.com>
2022-02-02 17:22:41 +0000

    fixing exercise 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,15 @@
  * main - Prints first alphabet in lower case and then prints in uppercase
  * Return: Always 0
  */
+
 int main(void)
 {
 char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++);
+for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-       
-for (letter = 'A'; letter <= 'Z'; letter++);
+for (letter = 'A'; letter <= 'Z'; letter++)
 putchar(letter);
-
 putchar('\n');
 
 return (0);
