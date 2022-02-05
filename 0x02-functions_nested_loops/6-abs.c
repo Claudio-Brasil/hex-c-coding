a816ac53db29fb7bea2f154cf8fe2aeea16367bc
Claudio <clfjr2002@gmail.com>
2022-02-05 13:13:09 +0000

    create 6 main and 6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @a: any integer
+ * Return: a for absolute integer
+ */
+int _abs(int a)
+{
+if (a < 0)
+retun (a = -a);
+else 
+return (a);
+}
