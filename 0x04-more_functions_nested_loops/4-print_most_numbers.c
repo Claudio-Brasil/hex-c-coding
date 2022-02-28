184ec65768a51bac78968cfe33b617a552060c5f
Claudio <clfjr2002@gmail.com>
2022-02-28 20:12:10 +0000

    exercise 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers 0-9 except 2 and 4
+ * Return: 0 for success
+ */
+int print_most_numbers(void)
+{
+char c;
+c = '0';
+
+while (c <= '9')
+{
+if (c != '2' || c != '4')
+{
+_putchar (c);
+}
+c++;
+}
+_putchar ('\n');
+}
