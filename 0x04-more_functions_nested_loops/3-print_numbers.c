8811c8400b22ec0b58c290a6b7947ba8cac22437
Claudio <clfjr2002@gmail.com>
2022-02-28 19:49:00 +0000

    exercise 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_numbers - prints numbers 0-9
+* Return: Always 0
+*/
+void print_numbers(void)
+{
+char c;
+
+c = '0';
+
+while (c <= '9')
+{
+_putchar (c);
+c++
+}
+_putchar('\n');
+}
+
