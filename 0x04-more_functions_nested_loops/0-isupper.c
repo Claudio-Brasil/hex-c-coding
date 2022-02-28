def4727003893a862a1ee5588b188663302953c7
Claudio <clfjr2002@gmail.com>
2022-02-28 19:01:47 +0000

    fix issuper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
  *_isupper - checks for uppercase letter
  * @c: uppercase letters
  * Return: 1 if c, 0 for everything else
  */
-int _issuper(int c)
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
