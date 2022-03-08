e0fc23fc645ec0accc6d391f203e4e4bd3532010
Claudio <clfjr2002@gmail.com>
2022-03-08 00:47:56 +0000

    print 0-9 with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* print_most_numbers - prints numbers 0-9 except 2 and 4
+* Return: 0 for success
+*/
+void print_most_numbers(void)
+{
+char c;
+
+c = '0';
+
+while (c <= '9')
+{
+if (!(c == '2' || c == '4'))
+{
+_putchar (c);
+}
+c++;
+}
+_putchar ('\n')
+}
