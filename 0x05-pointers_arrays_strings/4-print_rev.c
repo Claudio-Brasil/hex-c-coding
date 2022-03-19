46c969c83b50067beb2d2f1918ee2750f251bae5
Claudio <clfjr2002@gmail.com>
2022-03-19 15:41:10 +0000

    fix space error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,15 @@
 void print_rev(char *s)
 {
 int a, ln;
-for(ln = 0; *s != '\0'; s++)
+for (ln = 0; *s != '\0'; s++)
 ln++;
 
 s--;
 
-for(a = 0; a < ln; a++)
+for (a = 0; a < ln; a++)
 {
 _putchar(*s);
+
 s--;
 }
 _putchar('\n');
