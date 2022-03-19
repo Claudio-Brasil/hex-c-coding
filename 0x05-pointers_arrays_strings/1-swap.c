2b16d726aa130389b862e2dc32b8645154b7901b
Claudio <clfjr2002@gmail.com>
2022-03-19 14:50:55 +0000

    add main and exercise file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
