ffe09c44d9bffada38b79ccd7056407ce80b4233
Claudio <clfjr2002@gmail.com>
2022-03-29 15:56:45 +0000

    adding exercise 0 and README file

---
+++ 0x08-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+/**
+ * main - prints program name
+ * @argc: lenght of argv
+ * @argv: arguments array
+ * Return: it's name
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+if (argc)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
